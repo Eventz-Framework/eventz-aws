1b4cee0014b9bf35d4d6f928d34d4dec628e2a38
Dan Ballance <work@danballance.uk>
2020-11-27 22:13:44 +0000

    Change __fcn__ to __fqn__ in tests.

--- tests/test_event_store_json_s3.py
+++ tests/test_event_store_json_s3.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import boto3
 import pytest
 from eventz.aggregate import Aggregate
-from eventz.marshall import Marshall, DatetimeCodec
+from eventz.marshall import Marshall, DatetimeCodec, FqnResolver
 from moto import mock_s3
 
 from eventz_aws.event_store_json_s3 import EventStoreJsonS3
@@ -21,6 +21,18 @@ dt1 = datetime(2020, 1, 2, 3, 4, 5, 123456)
 dt2 = datetime(2020, 1, 2, 3, 4, 6, 123456)
 bucket_name = "TestBucket"
 region = "eu-west-1"
+marshall = Marshall(
+    fqn_resolver=FqnResolver(
+        fqn_map={
+            "tests.Child": "tests.example.child.Child",
+            "tests.Children": "tests.example.children.Children",
+            "tests.ChildChosen": "tests.example.parent.ChildChosen",
+            "tests.Parent": "tests.example.parent.Parent",
+            "tests.ParentCreated": "tests.example.parent.ParentCreated",
+        }
+    ),
+    codecs={"eventz.marshall.DatetimeCodec": DatetimeCodec()},
+)
 
 
 @mock_s3
@@ -30,12 +42,16 @@ def test_sequence_of_events_can_be_read(
     store = EventStoreJsonS3(
         bucket_name=bucket_name,
         region=region,
-        marshall=Marshall({"eventz.marshall.DatetimeCodec": DatetimeCodec()}),
+        marshall=marshall,
         recreate_storage=True,
     )
     # insert fixture data into the storage
     client = boto3.client("s3", region_name=region)
-    client.put_object(Bucket=bucket_name, Key=parent_id1, Body=json.dumps(json_events))
+    client.put_object(
+        Bucket=bucket_name,
+        Key=parent_id1,
+        Body=json.dumps(json_events, sort_keys=True, separators=(",", ":")),
+    )
     # run test and make assertion
     assert store.fetch(parent_id1) == (parent_created_event, child_chosen_event)
 
@@ -47,7 +63,7 @@ def test_new_sequence_of_events_can_be_persisted(
     store = EventStoreJsonS3(
         bucket_name=bucket_name,
         region=region,
-        marshall=Marshall({"eventz.marshall.DatetimeCodec": DatetimeCodec()}),
+        marshall=marshall,
         recreate_storage=True,
     )
     assert store.fetch(parent_id1) == ()
@@ -91,7 +107,7 @@ def child_chosen_event():
 def json_events():
     return [
         {
-            "__fcn__": "tests.example.parent.ParentCreated",
+            "__fqn__": "tests.ParentCreated",
             "__version__": 1,
             "msgid": msgid1,
             "timestamp": {
@@ -100,17 +116,17 @@ def json_events():
             },
             "parent_id": parent_id1,
             "children": {
-                "__fcn__": "tests.example.children.Children",
+                "__fqn__": "tests.Children",
                 "name": "Group One",
                 "items": [
-                    {"__fcn__": "tests.example.child.Child", "name": "Child One",},
-                    {"__fcn__": "tests.example.child.Child", "name": "Child Two",},
-                    {"__fcn__": "tests.example.child.Child", "name": "Child Three",},
+                    {"__fqn__": "tests.Child", "name": "Child One",},
+                    {"__fqn__": "tests.Child", "name": "Child Two",},
+                    {"__fqn__": "tests.Child", "name": "Child Three",},
                 ],
             },
         },
         {
-            "__fcn__": "tests.example.parent.ChildChosen",
+            "__fqn__": "tests.ChildChosen",
             "__version__": 1,
             "msgid": msgid2,
             "timestamp": {
@@ -118,6 +134,6 @@ def json_events():
                 "params": {"timestamp": dt_iso2},
             },
             "parent_id": parent_id1,
-            "child": {"__fcn__": "tests.example.child.Child", "name": "Child Three",},
+            "child": {"__fqn__": "tests.Child", "name": "Child Three",},
         },
     ]
