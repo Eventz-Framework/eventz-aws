a800eb370b1ec20bfe1c46ac4d8ed17c55edc412
Dan Ballance <work@danballance.uk>
2020-12-05 21:41:03 +0000

    Fix issue with EventPublisherDummy

--- eventz_aws/event_publisher_dummy.py
+++ eventz_aws/event_publisher_dummy.py
@@ -13,12 +13,4 @@ class EventPublisherDummy(EventPublisherProtocol):
         self._marshall: MarshallProtocol = marshall
 
     def publish(self, packet: Packet) -> None:
-        if isinstance(packet.payload, list):
-            for item in packet.payload:
-                self._try_event(item)
-            return
-        self._try_event(packet.payload)
-
-    def _try_event(self, item: Any) -> None:
-        if isinstance(item, Event):
-            self.events.append(self._marshall.to_json(item))
+        self.events.append(packet)
