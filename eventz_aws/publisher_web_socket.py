8a6f24c41592b6bc20fa98f8d64c7f81cafa5e26
Dan Ballance <work@danballance.uk>
2021-05-15 06:20:06 +0100

    Add SubscriptionRegistry to PublisherWebSocket & deregister failures

--- eventz_aws/publisher_web_socket.py
+++ eventz_aws/publisher_web_socket.py
@@ -3,17 +3,25 @@ import os
 
 from boto3_type_annotations.apigatewaymanagementapi import Client as WebSocketClient
 from eventz.packets import Packet
-from eventz.protocols import MarshallProtocol, PublisherProtocol
+from eventz.protocols import MarshallProtocol, PublisherProtocol, SubscriptionRegistryProtocol
 
 log = logging.getLogger(__name__)
 log.setLevel(os.getenv("LOG_LEVEL", "INFO"))
 
 
 class PublisherWebSocket(PublisherProtocol):
-    def __init__(self, marshall: MarshallProtocol, client: WebSocketClient):
+    def __init__(
+        self,
+        marshall: MarshallProtocol,
+        client: WebSocketClient,
+        subscription_registry: SubscriptionRegistryProtocol,
+    ):
         super().__init__()
         self._marshall: MarshallProtocol = marshall
         self._client: WebSocketClient = client
+        self._subscription_registry: SubscriptionRegistryProtocol = (
+            subscription_registry
+        )
 
     def publish(self, packet: Packet) -> None:
         message = {
@@ -29,7 +37,10 @@ class PublisherWebSocket(PublisherProtocol):
             message["payload"] = packet.payload
         log.info(f"message = {message}")
         for connection_id in packet.subscribers:
-            log.info(f"PublisherWebSocket.send connection_id={connection_id} packet={packet}")
-            self._client.post_to_connection(
-                ConnectionId=connection_id, Data=bytes(self._marshall.to_json(message), "utf-8")
-            )
+            try:
+                log.info(f"PublisherWebSocket.send connection_id={connection_id} packet={packet}")
+                self._client.post_to_connection(
+                    ConnectionId=connection_id, Data=bytes(self._marshall.to_json(message), "utf-8")
+                )
+            except self._client.exceptions.GoneException:
+                self._subscription_registry.deregister(subscription=connection_id)
